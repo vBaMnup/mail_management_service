7d5f6eec51b0a712198267975e91c557dbc26204
vbamnup <vbamnup@gmail.com>
2023-07-28 13:19:46 +0300

    rename app client to mailer

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "drf_yasg",
-    "src.client",
+    "src.mailing",
 ]
 
 MIDDLEWARE = [
--- src/mailing/admin.py
+++ src/mailing/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from src.client.models import Client
+from src.mailing.models import Client
 
 
 @admin.register(Client)
